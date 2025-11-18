23025cbe87f6642c3e79258cf2ab033d55ceb431
IrynaKr1 <krutsiryna@gmail.com>
2025-11-18 18:52:36 +0100

    Add formik to productForm

--- src/components/ProductForm/index.jsx
+++ src/components/ProductForm/index.jsx
@@ -1,4 +1,4 @@
-import { Formik } from 'formik';
+import { ErrorMessage, Field, Form, Formik } from 'formik';
 import React from 'react';
 import { PRODUCT_VALIDATION_SCHEMA } from '../../utils/validationSchemas';
 
@@ -18,11 +18,48 @@ function ProductForm () {
   };
 
   return (
-    <Formik
-      initialValues={initialValues}
-      onSubmit={submitHandler}
-      validationSchema={PRODUCT_VALIDATION_SCHEMA}
-    ></Formik>
+    <>
+      <h3>Add product to the list:</h3>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={submitHandler}
+        validationSchema={PRODUCT_VALIDATION_SCHEMA}
+      >
+        <Form>
+          <label>
+            Product Name:
+            <Field
+              name='productName'
+              type='text'
+              autoFocus
+              placeholder='Add product name'
+            />
+            <ErrorMessage name='productName' component='div' />
+          </label>
+          <label>
+            Producent:
+            <Field name='producent' type='text' placeholder='Add producent' />
+            <ErrorMessage name='producent' component='div' />
+          </label>
+          <label>
+            Price:
+            <Field name='price' type='number' placeholder='Add product price' />
+            <ErrorMessage name='price' component='div' />
+          </label>
+          <label>
+            Category:
+            <Field name='category' type='number' placeholder='Add category' />
+            <ErrorMessage name='category' component='div' />
+          </label>
+          <label>
+            Image:
+            <Field name='image' type='url' placeholder='Add image url' />
+            <ErrorMessage name='image' component='div' />
+          </label>
+          <button type='submit'>Add product</button>
+        </Form>
+      </Formik>
+    </>
   );
 }
 
