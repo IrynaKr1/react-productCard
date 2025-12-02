8ce9ad94c098450e36b0cabb166ae02462c60846
IrynaKr1 <krutsiryna@gmail.com>
2025-12-02 17:39:08 +0100

    Add footer

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,11 @@
+import styles from './Footer.module.scss';
+
+function Footer () {
+  return (
+    <div className={styles.footer}>
+      <p>All right reserved</p>
+    </div>
+  );
+}
+
+export default Footer;
--- src/pages/MainPage/index.jsx
+++ src/pages/MainPage/index.jsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import { Routes, Route } from 'react-router';
 import Header from '../../components/Header';
 import styles from './MainPage.module.scss';
+import ProductList from '../../components/ProductList';
+import Footer from '../../components/Footer';
 
 function MainPage () {
   return (
     <>
       <div className={styles.mainPage}>
-        <Header />
+        <Header className={styles.header} />
+        <main className={styles.mainContent}>
+          <Routes>
+            <Route path='/' element={<div>Home Page - TODO</div>} />
+            <Route path='/products' element={<ProductList />} />
+            <Route path='/admin' element={<div>Add Product Page - TODO</div>} />
+            <Route path='/users' element={<div>Users Page - TODO</div>} />
+          </Routes>
+        </main>
+        <Footer />
       </div>
     </>
   );
